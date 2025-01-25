0b86e9adb20c8eb5642ae299a903fbadec4e35fc
thexeromin <thexeromin@gmail.com>
2025-01-25 19:33:43 +0530

    update: ui

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,12 +1,19 @@
 import Base from '@/components/Base'
+import Feature from '@/components/Feature'
+import Footer from '@/components/Footer'
 import Hero from '@/components/Hero'
 
 export default function Home() {
     return (
-        <Base>
-            <main>
-                <Hero />
-            </main>
-        </Base>
+        <>
+            <Base>
+                <main>
+                    <Hero />
+                </main>
+
+                <Feature />
+            </Base>
+            <Footer />
+        </>
     )
 }
