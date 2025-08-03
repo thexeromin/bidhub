ddbd1b0ebdc988c6821cae4ba3c8ff141b341ea6
thexeromin <thexeromin@gmail.com>
2025-08-03 11:18:50 +0530

    fix: header nav links

--- client/src/components/layout/header.tsx
+++ client/src/components/layout/header.tsx
@@ -1,26 +1,32 @@
 'use client'
 import { useState } from 'react'
+import Link from 'next/link'
 
 export default function Header() {
     const [menuOpen, setMenuOpen] = useState(false)
+    const links = [
+        { url: '/', text: 'Home' },
+        { url: '/listings', text: 'Listings' },
+        { url: '/categories', text: 'Categories' },
+        { url: '/login', text: 'Login' },
+    ]
 
     return (
         <header className="bg-white shadow-sm sticky top-0 z-50">
             <div className="max-w-7xl mx-auto px-4 py-4 flex justify-between items-center">
-                <h1 className="text-3xl font-bold text-indigo-600">Bidhub</h1>
+                <h1 className="text-3xl font-bold text-indigo-600">
+                    <Link href="/">Bidhub</Link>
+                </h1>
                 <nav className="hidden md:flex space-x-8">
-                    <a href="#" className="hover:text-indigo-600">
-                        Home
-                    </a>
-                    <a href="#" className="hover:text-indigo-600">
-                        Listings
-                    </a>
-                    <a href="#" className="hover:text-indigo-600">
-                        Categories
-                    </a>
-                    <a href="/login" className="hover:text-indigo-600">
-                        Login
-                    </a>
+                    {links.map((link, idx) => (
+                        <Link
+                            href={link.url}
+                            className="hover:text-indigo-600 capitalize"
+                            key={link.text + idx}
+                        >
+                            {link.text}
+                        </Link>
+                    ))}
                 </nav>
                 <button
                     className="md:hidden text-2xl text-indigo-600 focus:outline-none"
